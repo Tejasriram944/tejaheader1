c9768ca5eeae532dfa4e319e9350681d6341b194
TEJA SRIRAM <49815246+Tejasriram944@users.noreply.github.com>
2020-04-01 13:31:35 +0530

    added files

---
+++ js/cust-script.js
@@ -0,0 +1,17 @@
+// var burger = document.getElementById('burger');
+
+var  navMenu = document.getElementById('navbar-menu');
+// navMenu.style.display = 'none';
+
+function myFunction() {
+    if(navMenu.style.display == 'none'){
+        navMenu.style.display = 'block';
+        
+   }
+   else if(navMenu.style.display == 'block'){
+    navMenu.style.display == 'none';
+   }
+   else{
+    navMenu.style.display == 'none'
+   }
+}
